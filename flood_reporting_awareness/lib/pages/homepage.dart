12a68172fdd4bfccb7e3647dbbc700cfa2645497
IrunguJoseph <irungujoseph07@gmail.com>
2024-11-28 10:15:14 +0300

    Added an image, text box and a button for the user to report floods in their area

--- flood_reporting_awareness/lib/pages/homepage.dart
+++ flood_reporting_awareness/lib/pages/homepage.dart
@@ -21,11 +21,64 @@ class _HomepageState extends State<Homepage> {
           title: const Center(
             child: Text(
               "FLood Reporting Awareness",
-              style: TextStyle(color: Colors.black38),
+              style: TextStyle(color: Colors.black),
             ),
           ),
+        ),
+        body: Column(
+          children: [
+            homeImage(),
+            SizedBox(height: deviceHeight! * 0.01),
+            bottomButtons(),
+          ],
         ),
       ),
     );
   }
+
+  Widget homeImage() {
+    return Image.asset(
+      'assets/images/road1.jpg',
+      width: deviceWidth,
+      );
+  }
+
+  Widget bottomButtons() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        bottomButtonsText(),
+        SizedBox(height: deviceHeight! * 0.05),
+        bottomButtonsReportFloods(),
+        SizedBox(height: deviceHeight! * 0.05),
+        //bottomButtonsVideoChat(),
+      ],
+    );
+  }
+
+   Widget bottomButtonsText() {
+  return Center(
+    child: Text(
+      "Ensure the road you are using is not flooded.",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: deviceWidth! * 0.1,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center, 
+    ),
+  );
+}
+
+ Widget bottomButtonsReportFloods() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue, // foreground
+      ),
+      onPressed: () {},
+      child: Text('Report Floods'),
+    );
+  }
+  
 }
